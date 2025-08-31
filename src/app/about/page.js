74ccea43a82e63d04a8f079e185107c7bfe4f23f
Stephen An <stephenfcan@gmail.com>
2025-08-31 01:21:08 -0600

    decrease font size on about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,10 +2,10 @@ export default function AboutPage() {
   return (
     <div className="bg-[#D9C4B1] text-[#5A3628] flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-4 sm:p-20 pt-20 sm:pt-0 -mt-14">
       <h1 className="font-Mirage text-5xl pb-3">about nujii</h1>
-      <p className="font-sans text-md md:text-lg text-center max-w-xl pb-3">
+      <p className="font-sans text-center max-w-xl pb-3">
         nujii is a community-inspired craft tea company based out of Denver, CO — available exclusively through pop-up experiences.
       </p>
-      <p className="font-sans text-md md:text-lg text-center max-w-xl">
+      <p className="font-sans text-center max-w-xl">
         our teas are sourced from the high mountain regions of Taiwan and China, offered strictly in loose-leaf form for the purest 
         expression of flavor. we carefully adjust the water&apos;s minerality to unlock each tea&apos;s ideal taste profile, while steeping with precise times, 
         temperatures, and ingredient ratios to ensure every cup is masterfully crafted.
