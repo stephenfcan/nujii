ff46415d942e407798797db37fe04e65e65d0f36
Stephen An <stephenfcan@gmail.com>
2025-11-17 17:34:07 -0700

    menu responsiveness

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -15,39 +15,39 @@ export default function MenuPage() {
             <div className="flex space-x-3 sm:space-x-5">
               <p>green tea</p>
               <p>moganshan mountain, china</p>
-              <p className="invisible sm:visible">2024 harvest</p>
-              <i className="invisible sm:visible">light, sweet, crisp</i>
+              <p className="hidden sm:flex">2024 harvest</p>
+              <i className="hidden sm:flex">light, sweet, crisp</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>rose red tea</p>
               <p>yunnan, taiwan</p>
-              <p className="invisible sm:visible">2022 harvest</p>
-              <i className="invisible sm:visible">bold, fruity</i>
+              <p className="hidden sm:flex">2022 harvest</p>
+              <i className="hidden sm:flex">bold, fruity</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>red tea</p>
               <p>nantou, taiwan</p>
-              <p className="invisible sm:visible">2025 harvest</p>
-              <i className="invisible sm:visible">malty with a hint of cinnamon</i>
+              <p className="hidden sm:flex">2025 harvest</p>
+              <i className="hidden sm:flex">malty with a hint of cinnamon</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>oolong tea</p>
               <p>alishan mountain, taiwan</p>
-              <p className="invisible sm:visible">2024 harvest</p>
-              <i className="invisible sm:visible">light, sweet, crisp</i>
+              <p className="hidden sm:flex">2024 harvest</p>
+              <i className="hidden sm:flex">light, sweet, crisp</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>oolong tea</p>
               <p>fujian, china</p>
-              <p className="invisible sm:visible">2025 harvest</p>
-              <i className="invisible sm:visible">bold, fruity</i>
+              <p className="hidden sm:flex">2025 harvest</p>
+              <i className="hidden sm:flex">bold, fruity</i>
             </div>
           </div>
         </div>
         <div className="flex space-x-5">
           {/* Images */}
-          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://private-user-images.githubusercontent.com/84104582/515399615-877a79e2-7bcb-411a-b98c-95c2c846093c.jpg?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM0MTUzMTAsIm5iZiI6MTc2MzQxNTAxMCwicGF0aCI6Ii84NDEwNDU4Mi81MTUzOTk2MTUtODc3YTc5ZTItN2JjYi00MTFhLWI5OGMtOTVjMmM4NDYwOTNjLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTExMTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUxMTE3VDIxMzAxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZmODExM2E5OTgwZDIzMjdiZDU1ZWVhYzg0NWEwMTBjNTk2YzQ4NTgzOGFlYTFhYzI4NTgwODYxZDdhZTE0MGQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.KUi2QiX7E32igRTWYbE8Jzg4J7YxorxPdlWAGmPcppQ" alt="" />
-          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://private-user-images.githubusercontent.com/84104582/515399623-28b5b4b5-c33b-48f9-81b0-0309790c6fb6.jpg?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM0MTUzMTAsIm5iZiI6MTc2MzQxNTAxMCwicGF0aCI6Ii84NDEwNDU4Mi81MTUzOTk2MjMtMjhiNWI0YjUtYzMzYi00OGY5LTgxYjAtMDMwOTc5MGM2ZmI2LmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTExMTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUxMTE3VDIxMzAxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTFiNDg2YzAyYTRlMjM5ZmFlODYyNzczOWJiODAxYzUyMmVkZDNlMGQ4YjQzZTI4NmViODhiYjdmYTkyMDkzZWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.0PGXuCynB4b44qIA9kMPOmYar0tuDog3BQXJ4fN9Cxg" alt="" />
+          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://github.com/user-attachments/assets/877a79e2-7bcb-411a-b98c-95c2c846093c" alt="" />
+          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://github.com/user-attachments/assets/28b5b4b5-c33b-48f9-81b0-0309790c6fb6" alt="" />
         </div>
       </div>
       {/* milk teas */}
@@ -58,33 +58,33 @@ export default function MenuPage() {
             <div className="flex space-x-3 sm:space-x-5">
               <p>green tea</p>
               <p>moganshan mountain, china</p>
-              <p className="invisible sm:visible">2024 harvest</p>
-              <i className="invisible sm:visible">light, sweet, crisp</i>
+              <p className="hidden sm:flex">2024 harvest</p>
+              <i className="hidden sm:flex">light, sweet, crisp</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>jasmine green tea</p>
               <p>sanjiang, china</p>
-              <p className="invisible sm:visible">2024 harvest</p>
-              <i className="invisible sm:visible">bright, floral, crisp</i>
+              <p className="hidden sm:flex">2024 harvest</p>
+              <i className="hidden sm:flex">bright, floral, crisp</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>rose red tea</p>
               <p>yunnan, taiwan</p>
-              <p className="invisible sm:visible">2022 harvest</p>
-              <i className="invisible sm:visible">bold, fruity</i>
+              <p className="hidden sm:flex">2022 harvest</p>
+              <i className="hidden sm:flex">bold, fruity</i>
             </div>
             <div className="flex space-x-3 sm:space-x-5">
               <p>oolong tea</p>
               <p>alishan mountain, taiwan</p>
-              <p className="invisible sm:visible">2024 harvest</p>
-              <i className="invisible sm:visible">light, sweet, crisp</i>
+              <p className="hidden sm:flex">2024 harvest</p>
+              <i className="hidden sm:flex">light, sweet, crisp</i>
             </div>
           </div>
         </div>
         <div className="flex space-x-5">
           {/* Images */}
-          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://private-user-images.githubusercontent.com/84104582/515399633-f8f7a239-e80d-46c0-9157-36b24490e6ec.jpg?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM0MTUzMTAsIm5iZiI6MTc2MzQxNTAxMCwicGF0aCI6Ii84NDEwNDU4Mi81MTUzOTk2MzMtZjhmN2EyMzktZTgwZC00NmMwLTkxNTctMzZiMjQ0OTBlNmVjLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTExMTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUxMTE3VDIxMzAxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTI2OTA1NzM4NWQyNTJhZDY2MDZlMDc1NWU3YzA3MzlkODNlYzdmNGJlZmFkNjkxNDI4Y2Q5NWFlYzgxNTU5MTImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.9dOexM_3dsQeZYq4GbefWCLUYdW3ax-V5W8xmuAs4CY" alt="" />
-          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://private-user-images.githubusercontent.com/84104582/515399626-6c0b8065-75f0-4f5b-bffd-cfdff7872f39.jpg?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM0MTUzMTAsIm5iZiI6MTc2MzQxNTAxMCwicGF0aCI6Ii84NDEwNDU4Mi81MTUzOTk2MjYtNmMwYjgwNjUtNzVmMC00ZjViLWJmZmQtY2ZkZmY3ODcyZjM5LmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTExMTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUxMTE3VDIxMzAxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPThmM2NlOGQxY2IzMzBmMTAzZDQ0MjIzNGU5MTg5ZDA4Y2FmMzE4NzhiYTk1ZDQ1NmU3YmY4NGE2OTA4YzIzMDkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.K8HUiRDhMPs8cGbFZi-TyA4ZNzMzUqjD8672WoH4atU" alt="" />
+          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://github.com/user-attachments/assets/f8f7a239-e80d-46c0-9157-36b24490e6ec" alt="" />
+          <img className='h-30 w-30 object-contain md:h-60 md:w-60' src="https://github.com/user-attachments/assets/6c0b8065-75f0-4f5b-bffd-cfdff7872f39" alt="" />
         </div>
       </div>
     </div>
