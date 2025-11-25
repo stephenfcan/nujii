9800b61e9df7017f4044d9998b10002a5f30c733
Stephen An <stephenfcan@gmail.com>
2025-11-24 19:55:38 -0700

    add jan 2026 popup

--- src/app/popups/page.js
+++ src/app/popups/page.js
@@ -3,17 +3,30 @@ export default function PopupsPage() {
     <div className="bg-white text-[#1a7533] p-8 pb-20 gap-4 sm:p-20 -mt-14">
       <div className="flex flex-col items-center justify-center min-h-screen -mt-20">
         <h1 className="font-Mirage text-5xl">upcoming pop-ups</h1>
-        <div className="mt-12">
-            <p className="font-sans text-md max-w-xl">Stay tuned!</p>
-            {/* <p className="font-sans text-md max-w-xl">
-                Havana Street Night Market - September 27
-            </p>
-            <p className="font-sans text-md max-w-xl">
-                6-10pm
-            </p>
-            <p className="font-sans text-md max-w-xl">
-                2710 S Havana St, Aurora, CO 80014
-            </p> */}
+        <div className="flex flex-col space-y-5 mt-12">
+            {/* <p className="font-sans text-md max-w-xl">Stay tuned!</p> */}
+            <div>
+              <p className="font-sans text-md max-w-xl">
+                Private Tea Tasting
+              </p>
+              <p className="font-sans text-md max-w-xl">
+                January 10, 1-3pm
+              </p>
+              <i className="font-sans text-md max-w-xl">
+                Private Location
+              </i>
+            </div>
+            {/* <div>
+              <p className="font-sans text-md max-w-xl">
+                2026 Lunar New Year on Havana Street
+              </p>
+              <p className="font-sans text-md max-w-xl">
+                February 15, 12-6pm
+              </p>
+              <i className="font-sans text-md max-w-xl">
+                The Stampede, Aurora
+              </i>
+            </div> */}
         </div>
       </div>
     </div>
