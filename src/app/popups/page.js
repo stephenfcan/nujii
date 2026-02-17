577ae28cf6153a212efc6244146855e8aae90d15
Stephen An <stephenfcan@gmail.com>
2026-02-16 19:38:23 -0700

    remove lny 2026

--- src/app/popups/page.js
+++ src/app/popups/page.js
@@ -2,9 +2,10 @@ export default function PopupsPage() {
   return (
     <div className="bg-white text-[#1a7533] p-8 pb-20 gap-4 sm:p-20 -mt-14">
       <div className="flex flex-col items-center justify-center min-h-screen -mt-20">
-        <h1 className="font-Mirage text-5xl mb-12">upcoming pop-ups</h1>
+        <h1 className="font-Mirage text-5xl mb-10">upcoming pop-ups</h1>
+          <p className="font-sans text-md max-w-xl mb-20">Stay tuned!</p>
+        {/* <h1 className="font-Mirage text-5xl mb-10">past events</h1>
         <div className="flex flex-col space-y-5 md:flex-row md:space-x-10">
-            {/* <p className="font-sans text-md max-w-xl">Stay tuned!</p> */}
             <div>
               <p className="font-sans text-md max-w-xl">
                 Private Tea Tasting
@@ -13,7 +14,7 @@ export default function PopupsPage() {
                 January 18, 1-3pm MST
               </p>
               <i className="font-sans text-md max-w-xl">
-                Private Location
+                Private Location, CO
               </i>
             </div>
             <div>
@@ -27,7 +28,7 @@ export default function PopupsPage() {
                 Stampede, CO
               </i>
             </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
